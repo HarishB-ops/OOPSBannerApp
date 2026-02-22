00a400af3bfec0a496b5132d8925557dcff66ffb
unknown <harishbalamurugan254@gmail.com>
2026-02-22 22:02:06 +0530

    UC3 : BannerAppStingJoin

---
+++ BannerAppStringJoin.java
@@ -0,0 +1,48 @@
+public class BannerAppStringJoin {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                "***",
+                "*****",
+                "*****",
+                "*****"
+        ));
+
+        System.out.println(String.join(" ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*"
+        ));
+
+        System.out.println(String.join(" ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "***"
+        ));
+
+        System.out.println(String.join(" ",
+                "*   *",
+                "*****",
+                "*",
+                "*"
+        ));
+
+        System.out.println(String.join(" ",
+                "*   *",
+                "*",
+                "*",
+                "*   *"
+        ));
+
+        System.out.println(String.join(" ",
+                "***",
+                "*",
+                "*",
+                "***"
+        ));
+
+    }
+}
