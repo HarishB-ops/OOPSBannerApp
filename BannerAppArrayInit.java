691d3b306bbc3f9dd68dcc02b569871a2cc39a75
unknown <harishbalamurugan254@gmail.com>
2026-03-03 23:02:32 +0530

    UC6-staticfunction

---
+++ BannerAppArrayInit.java
@@ -0,0 +1,21 @@
+public class BannerAppArrayInit {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+
+            String.join(" ", " *** ", " ***** ", " ***** ", " ***** "),
+            String.join(" ", "*   *", "*     *", "*     *", "*     "),
+            String.join(" ", "*   *", "*     *", "*     *", "*     "),
+            String.join(" ", "*   *", "*     *", " ***** ", " ***** "),
+            String.join(" ", "*   *", "*     *", "     * ", "     * "),
+            String.join(" ", "*   *", "*     *", "     * ", "     * "),
+            String.join(" ", " *** ", " ***** ", " ***** ", " ***** ")
+
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
